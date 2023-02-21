ed67c3bd02864dc69f49ebcbbd0fedd1f94a275c
Jeybriel Townsend <33335469+townsendjt19@users.noreply.github.com>
2023-02-21 12:18:25 -0500

    javascript practice

---
+++ HTMLpractice/javaScriptPrac/javascript.js
@@ -0,0 +1,60 @@
+console.log("Hello World!");
+console.log("Hello World!!!");
+
+
+let numOfCups=0 ; 
+
+if (numOfCups<3){
+  
+    console.log("Yes I'll take another cup of coffee.");
+} else {
+    
+    console.log("I think I'm okay for now.") ;
+}
+
+//logic is kinda like java though.
+
+//useful to use the let charaters for each type of variable.
+
+let temp = 80;
+
+console.log("The temperature is 80 degrees.")
+
+if (temp>80){
+    console.log("time to swim")
+    temp = 60; temp = 90; 
+}
+
+
+let precip = "raining"; 
+ 
+if (temp>80&&!raining){
+
+}
+let indoors = true; 
+
+if (indoors ||( temp>80 && !precip)){
+
+    console.log("time to swim");
+}
+
+
+//let bottlesOnWall = 99; 
+
+for (let bottlesOnWall = 99;bottlesOnWall>=0;bottlesOnWall--){
+
+    
+    
+    if (bottlesOnWall==0){
+
+     console.log("Hey! We need more root beer!")
+    }
+    else {
+        console.log(bottlesOnWall + " bottles of root beer on the wall")
+    }
+}
+
+//let y =66+6;
+//console.log(y);
+
+//console.log(y + "6");
